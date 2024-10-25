4b0c16facba918320ad8633c4b8a4e48185f898b
Nguyễn Mạnh Cường <77909327+manhcuong02@users.noreply.github.com>
2024-10-25 09:25:21 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,62 +1,81 @@
-import matplotlib.pyplot as plt
-import IPython.display as ipd
-
-import os
 import json
 import math
+import os
+import time
+
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import soundfile as sf
 import torch
+from scipy.io.wavfile import write
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import AutoTokenizer, PreTrainedTokenizer, T5ForConditionalGeneration
+from underthesea import word_tokenize
 
 import commons
 import utils
-from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from data_utils import (
+    TextAudioCollate,
+    TextAudioLoader,
+    TextAudioSpeakerCollate,
+    TextAudioSpeakerLoader,
+)
 from models import SynthesizerTrn
-from transformers import AutoTokenizer, T5ForConditionalGeneration
-from scipy.io.wavfile import write
-import soundfile as sf
 from text2phonemesequence import Text2PhonemeSequence
-from tqdm import tqdm
+from vinorm import TTSnorm
 
 
-def get_inputs(text, model, tokenizer_xphonebert):
+def get_inputs(
+    text, model: Text2PhonemeSequence, tokenizer_xphonebert: PreTrainedTokenizer
+):
     phones = model.infer_sentence(text)
     tokenized_text = tokenizer_xphonebert(phones)
-    input_ids = tokenized_text['input_ids']
-    attention_mask = tokenized_text['attention_mask']
-    input_ids = torch.LongTensor(input_ids).cuda()
-    attention_mask = torch.LongTensor(attention_mask).cuda()
+    input_ids = tokenized_text["input_ids"]
+    attention_mask = tokenized_text["attention_mask"]
+    input_ids = torch.LongTensor(input_ids).to("cuda:1")
+    attention_mask = torch.LongTensor(attention_mask).to("cuda:1")
     return input_ids, attention_mask
 
-hps = utils.get_hparams_from_file("./configs/lj_base_xphonebert.json")
+
+hps = utils.get_hparams_from_file("configs/infore_25h_base_xphonebert.json")
 tokenizer_xphonebert = AutoTokenizer.from_pretrained(hps.bert)
 # Load Text2PhonemeSequence
-model = Text2PhonemeSequence(language='eng-us', is_cuda=True)
+model = Text2PhonemeSequence(language="vie-n", device="cuda:1")
 net_g = SynthesizerTrn(
     hps.bert,
     hps.data.filter_length // 2 + 1,
     hps.train.segment_size // hps.data.hop_length,
-    **hps.model).cuda()
+    **hps.model
+).to("cuda:1")
 _ = net_g.eval()
 
-_ = utils.load_checkpoint("./logs/lj_base_xphonebert/G_161200.pth", net_g, None)
+text = "Lê Quý Đôn, tên thuở nhỏ là Lê Danh Phương, là vị quan thời Lê trung hưng, cũng là nhà thơ và được mệnh danh là nhà bác học lớn của Việt Nam trong thời phong kiến."
+norm_text = TTSnorm(text)
+text_segment = word_tokenize(norm_text, format="text")
+print(text_segment)
 
-f = open('./filelists/ljs_audio_text_test_filelist_phoneme_sequence.txt', 'r')
-list_lines = f.readlines()
-f.close()
-for line in tqdm(list_lines):
-    line = line.strip().split('|')
-    assert len(line) == 2
+_ = utils.load_checkpoint("logs/infore_25h_p2//G_1220000.pth", net_g, None)
 
-    stn_tst, attention_mask = get_text(line[-1], model, tokenizer_xphonebert)
-    with torch.no_grad():
-        x_tst = stn_tst.cuda().unsqueeze(0)
-        attention_mask = attention_mask.cuda().unsqueeze(0)
-        audio = net_g.infer(x_tst, attention_mask, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+# text_segment = "hiện_nay vị_trí của bàn_thờ thường được đặt trong phòng riêng ở tầng trên cùng của nhà ."
 
-    sf.write('./out_ljspeech/' + line[0], audio, hps.data.sampling_rate)
-    ipd.display(ipd.Audio(audio, rate=hps.data.sampling_rate, normalize=False))
+start = time.time()
+stn_tst, attention_mask = get_inputs(text_segment, model, tokenizer_xphonebert)
+with torch.no_grad():
+    x_tst = stn_tst.unsqueeze(0).to("cuda:1")
+    attention_mask = attention_mask.to("cuda:1").unsqueeze(0)
+
+    audio = net_g.infer(
+            x_tst,
+            attention_mask,
+            noise_scale=0.667,
+            noise_scale_w=0.8,
+            length_scale=1,
+        )[0][0, 0].data.cpu().float().numpy()
     
+print("Time: ", time.time() - start)
 
+sf.write('output.wav', audio, hps.data.sampling_rate)
 
